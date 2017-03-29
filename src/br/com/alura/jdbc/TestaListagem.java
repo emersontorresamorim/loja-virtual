76f44b34ca590a84f9cd4f932a48d440281e233d
Emerson Torres <emerson.torres.tec@gmail.com>
2017-03-29 11:47:21 -0300

    Metodo para testar conexao JDBC

--- src/br/com/alura/jdbc/TestaListagem.java
+++ src/br/com/alura/jdbc/TestaListagem.java
@@ -1,8 +1,28 @@
 package br.com.alura.jdbc;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class TestaListagem {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
+		Connection connection = DriverManager.getConnection("jdbc:hsqldb:hsql://localhost/loja-virtual", "SA", "");
+		Statement statement = connection.createStatement();
+		statement.execute("SELECT * FROM produto");
+		ResultSet resultSet = statement.getResultSet();
 		
+		while (resultSet.next()) {
+			int id = resultSet.getInt("id");
+			String nome = resultSet.getString("nome");
+			String descricao = resultSet.getString("descricao");
+			System.out.println("ID: " + id + " - Nome: " + nome + " - Descrição: " + descricao);
+		}
+		
+		resultSet.close();
+		statement.close();
+		connection.close();
 	}
 }
